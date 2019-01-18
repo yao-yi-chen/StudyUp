1e2ae727b4416752a36380e22d59f76943e7dd58
yao-yi-chen <46581866+yao-yi-chen@users.noreply.github.com>
2019-01-17 23:09:05 -0800

    Add files via upload
    add location object

---
+++ StudyUp/src/map/Location.java
@@ -0,0 +1,40 @@
+package map;
+
+public class Location {
+
+	private double lat;
+	private double lon;
+
+
+
+	public Location(double lat, double lon) {
+		// TODO Auto-generated constructor stub
+		public double getLat() {
+
+			return lat;
+
+			}
+ 
+
+			public void setLat(double lat) {
+
+			this.lat = lat;
+
+			}
+
+			 
+
+			public double getLon() {
+
+			return lon;
+
+			}
+
+			 
+
+			public void setLon(double lon) {
+
+			this.lon = lon;
+	}
+
+}
